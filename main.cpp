281cfa46e5eb3a210292bb4af3965d12239b1bcb
frozenlyyy <ghost_fokin@mail.ru>
2024-11-20 01:36:30 +0300

    Added sum calculation

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "sdt.h"
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cout << "Enter two integers: ";
+    std::cin >> a >> b;
+    std::cout << "Sum: " << a + b << std::endl;
+    return 0
+}
