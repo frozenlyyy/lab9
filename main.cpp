fa25e61fd8842e133b11a18d6d3c706205ec46e2
frozenlyyy <ghost_fokin@mail.ru>
2024-11-20 01:59:39 +0300

    Added educational comment

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main() {
     std::cout << "Enter two integers: ";
     std::cin >> a >> b;
     std::cout << "Sum: " << a + b << std::endl;
+    // This is an educational program
     return 0;
 }
